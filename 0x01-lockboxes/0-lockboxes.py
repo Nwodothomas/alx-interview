de61e1ecbd1f9b1b28cf8613c9593e2850ff2e44
Nwodo Thomas <Nwodothomas@gmail.com>
2023-07-07 11:52:00 +0000

    updated 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
 
-"""
+'''
     Determines if all boxes can be opened.
 
     Args: boxes (list): A list of lists representing the boxes.
 
     Returns: bool: True if all boxes can be opened, False otherwise.
-"""
+'''
 
 def canUnlockAll(boxes):
     
+''' Check if the boxes list is empty '''
     if len(boxes) == 0:
         return False
 
-    keys = [0]  # Start with the key to the first box
-    visited = [False] * len(boxes)  # Track visited boxes
+''' Start with the key to the first box '''
+
+    keys = [0]
+ 
+'''Track visited boxes ''' 
+    visited = [False] * len(boxes) 
 
     while keys:
         box = keys.pop()
@@ -25,5 +30,4 @@ def canUnlockAll(boxes):
                 keys.append(key)
 
     return all(visited)
-
 
