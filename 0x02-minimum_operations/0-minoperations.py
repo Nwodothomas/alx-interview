acaaebbf1cdc353bfcbe1739f7ddcb23d3ef5d03
Nwodo Thomas <Nwodothomas@gmail.com>
2023-07-11 14:37:15 +0000

    updated 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 
-''' 
-    A module that returns the minimum Operations it takes to
+''' A module that returns the minimum Operations it takes to
     get to n characters.
 
     Available operations:
