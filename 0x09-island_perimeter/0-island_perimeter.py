ef2caaae2ccf621c36e23507d6bdbb6b7c5b8a6f
Nwodo Thomas <Nwodothomas@gmail.com>
2023-08-30 02:52:03 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,45 +1,54 @@
 #!/usr/bin/python3
-""" Island Perimeter 
-"""
+"""Island Perimeter - ALX Interview"""
+
+
+def check_val(x):
+    """_summary_
+
+    Args:
+        n (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if (x == 0):
+        return 1
+    return 0
 
 
 def island_perimeter(grid):
-    """ return the perimeter of the island described in grid 
+    """_summary_
+
+    Args:
+        grid (_type_): _description_
     """
-    total_perimeter = 0
 
-    for i, row in enumerate(grid):
-        for j, element in enumerate(row):
-            # Check if element is land or sea
-            if (element == 0):
-                continue
+    row = len(grid)
+    col = len(grid[0])
+    assert (1 <= row and col <= 100), "length must be between 1 an 100"
 
-            # Left check
-            if (j != 0 and row[j - 1] == 0):
-                total_perimeter += 1
-            if (j == 0):
-                # left edge case
-                total_perimeter += 1
+    x = 0
+    for i in range(row):
+        for j in range(col):
+            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
+                "grid numbers must be 0 or 1"
+            if grid[i][j] == 1:
+                if i-1 < 0:
+                    x += 1
+                else:
+                    x += check_val(grid[i-1][j])
+                if j-1 < 0:
+                    x += 1
+                else:
+                    x += check_val(grid[i][j-1])
 
-            # Right check
-            if (j != len(row) - 1 and row[j + 1] == 0):
-                total_perimeter += 1
-            if (j == len(row) - 1):
-                # right edge case
-                total_perimeter += 1
+                try:
+                    x += check_val(grid[i+1][j])
+                except IndexError:
+                    x += 1
+                try:
+                    x += check_val(grid[i][j+1])
+                except IndexError:
+                    x += 1
 
-            # Upper check
-            if (i != 0 and grid[i - 1][j] == 0):
-                total_perimeter += 1
-            if (i == 0):
-                # top edge case
-                total_perimeter += 1
-
-            # Bottom Check
-            if (i != len(grid) - 1 and grid[i + 1][j] == 0):
-                total_perimeter += 1
-            if (i == len(grid) - 1):
-                # bottom edge case
-                total_perimeter += 1
-
-    return total_perimeter
+    return x
