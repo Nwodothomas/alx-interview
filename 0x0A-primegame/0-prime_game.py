6260146874eedd591331b3985a97fdcf13301e6f
Nwodo Thomas <Nwodothomas@gmail.com>
2023-09-06 23:10:00 +0100

    added 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,45 +1,29 @@
 #!/usr/bin/python3
-
-def is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-def calculate_primes_sieve(n):
-    sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False
-
-    for current in range(2, int(n**0.5) + 1):
-        if sieve[current]:
-            for multiple in range(current * current, n + 1, current):
-                sieve[multiple] = False
-
-    primes = [i for i, is_prime in enumerate(sieve) if is_prime]
-    return primes
+""" Module for solving prime game question """
 
 def isWinner(x, nums):
-    if x <= 0 or not nums:
+    """function that checks for the winner"""
+    if not nums or x < 1:
         return None
-
     max_num = max(nums)
-    primes = calculate_primes_sieve(max_num)
-    
-    maria_wins = 0
-    ben_wins = 0
 
-    for num in nums:
-        prime_count = sum(1 for p in primes if p <= num)
-        if prime_count % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    my_filter = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not my_filter[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            my_filter[j] = False
+    my_filter[0] = my_filter[1] = False
+    y = 0
+    for i in range(len(my_filter)):
+        if my_filter[i]:
+            y += 1
+        my_filter[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += my_filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
         return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
